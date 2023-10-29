2982ce651689f3a90886e8edbf3765fd79dea40a
Antje Stiller <antjestiller@gmx.de>
2023-10-29 12:46:06 +0100

    restructuring askedQuestion --> answeredQuestions

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,32 @@ import './App.scss'
 export default function App() {
 
   const [allQuestions, setAllQuestions] = React.useState([])
-  const [question, setQuestion] = React.useState({
-    id: "",
-    question: "...",
-    correctAnswer: "...",
-    allAnswers: [],
-    selected: "",
-    wasAsk:  false
-  })
+  const [question, setQuestion] = React.useState()
 
   // State of asked Questions && rounds
-  const [askedQuestions, setAskedQuestions] = React.useState([])
+  const [answeredQuestions, setAnsweredQuestions] = React.useState([])
   const [isCorrect, setIsCorrect] = React.useState(null)
   const [start, setStart] = React.useState(true)
   const [restart, setRestart] = React.useState(false)
 
   const score = [
-    {value: "0", milestone: true},
-    {value: "100", milestone: true},
-    {value: "200", milestone: false},
-    {value: "300", milestone: false},
-    {value: "500", milestone: false},
-    {value: "1.000", milestone: true},
-    {value: "2.000", milestone: false},
-    {value: "4.000", milestone: false},
-    {value: "8.000", milestone: false},
-    {value: "16.000", milestone: false},
-    {value: "32.000", milestone: true},
-    {value: "64.000", milestone: false},
-    {value: "125.000", milestone: false},
-    {value: "250.000", milestone: false},
-    {value: "500.000", milestone: false},
-    {value: "1.000.000", milestone: true}
+    {value: 0, milestone: true},
+    {value: 100, milestone: true},
+    {value: 200, milestone: false},
+    {value: 300, milestone: false},
+    {value: 500, milestone: false},
+    {value: 1000, milestone: true},
+    {value: 2000, milestone: false},
+    {value: 4000, milestone: false},
+    {value: 8000, milestone: false},
+    {value: 16000, milestone: false},
+    {value: 32000, milestone: true},
+    {value: 64000, milestone: false},
+    {value: 125000, milestone: false},
+    {value: 250000, milestone: false},
+    {value: 500000, milestone: false},
+    {value: 1000000, milestone: true}
   ]
-
 
   // Combine Answers randomly
   function insertAtRandomIndex(arr, item) {
@@ -69,8 +61,7 @@ export default function App() {
             correctAnswer: result.correct_answer,
             allAnswers: allAnswersArray,
             selected: "",
-            id:nanoid(),
-            wasAsk: false
+            id:nanoid()
           }
         }))
       }
@@ -79,10 +70,9 @@ export default function App() {
 
     // Get question from allQuestions Arr
     function getQuestion() {
-      console.log(allQuestions)
-      console.log(askedQuestions)
       setStart(false)
-      const unansweredQuestions = allQuestions.filter(q => q.wasAsk === false);
+
+      const unansweredQuestions = allQuestions.filter((q) => !answeredQuestions.includes(q.id))
 
       if (unansweredQuestions.length > 0) {
         const randomNumber = Math.floor(Math.random() * unansweredQuestions.length)
@@ -93,27 +83,18 @@ export default function App() {
           question: randomQuestion.question,
           correctAnswer: randomQuestion.correctAnswer,
           allAnswers: randomQuestion.allAnswers,
-          wasAsk: !randomQuestion.wasAsk,
           selected: "",
           givenAnswer: null
         })
 
-        setAskedQuestions([...askedQuestions, {...randomQuestion, wasAsk: true}])
+        setAnsweredQuestions([...answeredQuestions, {...randomQuestion}])
         setIsCorrect(null);
-        //wasAsk(question.id)
 
       } else {
         // Start new game
         console.log("finished")
       }
     }
-
-    // Handle givenAnswer on askedQuestion && update -wasAsk- on allQuestions
-    React.useEffect(()=>{
-      setAllQuestions(prevQuestions => prevQuestions.map(q => q.id === question.id ? {...q, wasAsk: question.wasAsk} : q))
-      setAskedQuestions(prevAskQ => prevAskQ.map(q => q.id === question.id ? {...q, givenAnswer: isCorrect} : q))
-    }, [question, isCorrect])
-
 
     function handleOptionSelect(selectedOption) {
       const isCorrectAnswer = selectedOption === question.correctAnswer;
@@ -125,12 +106,24 @@ export default function App() {
       }))
     }
 
-    /* function restartGame() {
-      setAskedQuestions([])
+    //Handle givenAnswer on askedQuestion after answer was selected (=== question.selected changes)
+    React.useEffect(() => {
+    setAnsweredQuestions(prevAnsweredQ => {
+        return prevAnsweredQ.map(a => {
+            return a.id === question.id ? {...a, givenAnswer: question.givenAnswer}: a
+            })
+    })
+    }, [question])
+
+
+
+    {/*
+    function restartGame() {
       setAllQuestions([])
+      setAnsweredQuestions([0])
       setRestart(prevState => !prevState)
-      getQuestion()
-    } */
+    }
+    */}
 
 
   return (
@@ -145,7 +138,7 @@ export default function App() {
           ?
           // Code for the start screen
           <div className="start-screen-container">
-            <p>you want to be a quizzionaire?</p><p>Put you knowledge to the test!</p>
+            <p>you want to be a quizzionaire?</p><p>Put your knowledge to the test!</p>
             <div className="btn">
                 <a href="#" onClick={getQuestion}>Start</a>
             </div>
@@ -154,8 +147,8 @@ export default function App() {
           // Code for the active game screen
           <div className="quiz-container">
             <div className="game-details-container">
-                <h1><span id="player-score" style={score[askedQuestions.length].milestone ? {color: `#FEC355`} : null }>  ${score[askedQuestions.length].value}</span></h1>
-                <h1><span id="question-number">{askedQuestions.length} / {allQuestions.length}</span></h1>
+                <h1><span id="player-score" style={score[0].milestone ? {color: `#FEC355`} : null }>  ${score[0].value}</span></h1>
+                <h1><span id="question-number">{answeredQuestions.length} / {allQuestions.length}</span></h1>
             </div>
             <Question
               key={question.id}
@@ -163,19 +156,18 @@ export default function App() {
               question={question.question}
               correctAnswer={question.correctAnswer}
               allAnswers={question.allAnswers}
-              wasAsk={question.wasAsk}
               handleSelect={handleOptionSelect}
               isCorrect={isCorrect}
               selected={question.selected}
             />
             {/* Conditional rendering of buttons */}
-            {question.selected === "" ? null : question.selected === question.correctAnswer ? (
+            {question.selected === "" ? null : answeredQuestions.length < allQuestions.length ? (
               <div className="btn">
                 <a className="start-button" onClick={getQuestion}>Next Question</a>
               </div>
             ) : (
               <div className="btn">
-                <a className="restart-button" onClick={getQuestion}>Next Question</a>
+                <a className="restart-button">Restart</a>
               </div>
             )}
           </div>
